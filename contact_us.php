72a92bdb0fd08b74288919e198d12e9e861462ef
sara-cameron-richard <134742947+sara-cameron-richard@users.noreply.github.com>
2023-06-09 14:34:50 -0400

    I updated the code
    Added the file structure!

---
+++ contact_us.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Contact Us - Web Design Specialist Program</title>
+  </head>
+  <body>
+    <div class="container">
+        <a href="index.php" class="btn btn-dark">Home</a>
+        <a href="contact_us.php" class="btn btn-dark">Contact</a>
+        <a href="map.php" class="btn btn-dark">Map</a>
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4"><img src="assets/images/phone.jpg" alt="COllege" width="100%"></div>
+            <div class="col-4"></div>
+        </div>
+        <h1>Contact Us - Web Design Specialist Program</h1>
+        <p>
+            The Website Design Specialist ACS/AEC – NWE.0F program aims to train website design specialists capable of designing, developing and maintaining websites. Graduates can produce interactive websites and its programming interfaces. They can intervene at various levels in the entire website production chain. Their work can involve design, programming (front-end and back-end), databases, publishing and project management. Graduates become versatile UX/UI website designers. They will be able to successfully integrate into the labour market and into multidisciplinary teams.
+        </p>
+        <h2>Program Approach</h2>
+        <p>
+            The program design follows three orientations to train specialists in website development:
+            <ol>
+                <li>Provides a practical approach to train students in website design environments.</li>
+                <li>Uses technology watch mechanisms to continually update the skills developed by students</li>
+                <li>Incorporates a workplace internship to enable students to apply and validate their competencies in an employment setting.</li>
+            </ol>
+        </p>
+    </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ map.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Map - Web Design Specialist Program</title>
+  </head>
+  <body>
+    <div class="container">
+        <a href="index.php" class="btn btn-dark">Home</a>
+        <a href="contact_us.php" class="btn btn-dark">Contact</a>
+        <a href="map.php" class="btn btn-dark">Map</a>
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4"><img src="assets/images/map.jpg" alt="COllege" width="100%"></div>
+            <div class="col-4"></div>
+        </div>
+        <h1>Map - Web Design Specialist Program</h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37661.51352790657!2d-73.65829495663685!3d45.52680026709554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91944caab728d%3A0x9aac470926dbbd6a!2sVanier%20College%20-%20Park-Extension%20Campus!5e0!3m2!1sen!2sca!4v1684528402850!5m2!1sen!2sca" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
